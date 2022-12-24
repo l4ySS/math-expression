9304b4064777145aeaf7e5f48dc2df8811bd3341
laysstrongmax <laysstrongmax@gmail.com>
2022-12-24 06:58:23 +0300

    ...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.List;
+
+/**
+ * Основной класс.
+ * @author  Slivkin Sergey
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * Метод, проводящий вычисления со строкой
+     * математического выражения и выводящий результат в консоль.
+     * @param args Не используется.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExpressionCalc expressionChecker = new ExpressionCalc();
+        String expressionText = "((20+2)+20)/2";
+        List<ExpressionCalc.Lexeme> lexemes = ExpressionCalc.lexAnalyze(expressionText);
+        ExpressionCalc.LexemeBuffer lexemeBuffer = new ExpressionCalc.LexemeBuffer(lexemes);
+        System.out.println(ExpressionCalc.expr(lexemeBuffer));
     }
 }
